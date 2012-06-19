10ed8e65bae830468ba69fe041b2bcf866c131f1
brooksryba <brooks@sightmachine.com>
2012-06-19 15:16:03 -0400

    Added a setup.py file to install the module

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+import os
+
+setup(
+    name="piggyphoto",
+    version="1.0.0",
+    description="DSLR camera control through python",
+    packages=['piggyphoto']
+)    
